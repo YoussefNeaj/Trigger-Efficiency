745ec8bb2f4ce1c32b197529a782d216e6a318cf
YoussefNeaj <43187634+YoussefNeaj@users.noreply.github.com>
2019-08-09 14:39:49 +0200

    Update efficiency.C

--- efficiency.C
+++ efficiency.C
@@ -12,8 +12,8 @@ using namespace std;
 
 void eff::Loop()
 {
-  //  ofstream outfile;
-  //outfile.open("efficiency.txt", ios::app); 
+  ofstream outfile;
+  outfile.open("efficiency.txt", ios::app); 
   Long64_t nentries = fChain->GetEntriesFast();
   //  cout << "Nentries is:" << nentries << endl;
   Long64_t nbytes = 0, nb = 0;
@@ -25,7 +25,7 @@ void eff::Loop()
     if (ientry < 0) break;
     nb = fChain->GetEntry(jentry);   nbytes += nb;
 	
-    //int n_passed = 0;
+    int n_passed = 0;
     if(HLT_xe110_pufit_L1XE50 == 1)
       {
 	n_passed++;
@@ -33,26 +33,19 @@ void eff::Loop()
     
   }
   //Efficiency:
-  //float eff = (n_passed/nentries);
+  float eff = (n_passed/nentries);
   //cout << "n_passed is: " << n_passed << endl;
-  //cout << "Efficiency is:" << eff << endl;
-  //outfile << eff << "\n";
-  cout << n_passed << endl; 
+  cout << "Efficiency is:" << eff << endl;
+  outfile << eff << "\n"; 
 }
-
-
-
-
-
 
 int efficiency(){
   ifstream samples;
-  //  ofstream outfile;
+  ofstream outfile;
   string line;
   
-  
   samples.open("samples.txt");
-  //outfile.open("efficiency.txt", "WRITE");
+  outfile.open("efficiency.txt", "WRITE");
   if(!samples)
     {
       cout << "SAMPLE FILE NOT OPEN" << endl;
@@ -68,7 +61,7 @@ int efficiency(){
       analysis->Loop();
       
     }
-  //samples.close();
-  //outfile.close();
+  samples.close();
+  outfile.close();
   return 0;
 }
